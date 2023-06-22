9d5b653a4daa6bfeb1aec689ad5fd0392be7444c
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-06-22 12:11:38 +0300

    Add arrow seriealization to plugin v3 proto

---
+++ pb/plugin/v3/arrow_test.go
@@ -0,0 +1,53 @@
+package plugin
+
+import (
+	"testing"
+
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/array"
+	"github.com/apache/arrow/go/v13/arrow/memory"
+)
+
+func TestSchemaRoundTrip(t *testing.T) {
+	md := arrow.NewMetadata([]string{"foo", "bar"}, []string{"baz", "quux"})
+	sc := arrow.NewSchema([]arrow.Field{
+		{Name: "a", Type: arrow.PrimitiveTypes.Int64},
+		{Name: "b", Type: arrow.PrimitiveTypes.Float64},
+	}, &md)
+	b, err := SchemaToBytes(sc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sc2, err := NewSchemaFromBytes(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !sc.Equal(sc2) {
+		t.Errorf("expected %v, got %v", sc, sc2)
+	}
+}
+
+func TestRecordRoundTrip(t *testing.T) {
+	md := arrow.NewMetadata([]string{"foo", "bar"}, []string{"baz", "quux"})
+	sc := arrow.NewSchema([]arrow.Field{
+		{Name: "a", Type: arrow.PrimitiveTypes.Int64},
+		{Name: "b", Type: arrow.PrimitiveTypes.Float64},
+	}, &md)
+	bldr := array.NewRecordBuilder(memory.DefaultAllocator, sc)
+	bldr.Field(0).(*array.Int64Builder).AppendValues([]int64{1, 2, 3}, nil)
+	bldr.Field(1).(*array.Float64Builder).AppendValues([]float64{1.1, 2.2, 3.3}, nil)
+	rec := bldr.NewRecord()
+	defer rec.Release()
+	
+	b, err := RecordToBytes(rec)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rec2, err := NewRecordFromBytes(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !array.RecordEqual(rec, rec2) {
+		t.Errorf("expected %v, got %v", rec, rec2)
+	}
+}
