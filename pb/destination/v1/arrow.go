66780042358299e25d0e2d30f4ecd49c15766f77
Yevgeny Pats <yev.pats@gmail.com>
2023-06-23 16:47:17 +0300

    fix: Update schema encoding/deocoding in v2 (#28)
    Follow-up to https://github.com/cloudquery/plugin-pb-go/pull/26. We shouldn't introduce any new objects here and those helper function should only take care of encoding/decoding.

--- pb/destination/v1/arrow.go
+++ pb/destination/v1/arrow.go
@@ -7,30 +7,7 @@ import (
 	"github.com/apache/arrow/go/v13/arrow/ipc"
 )
 
-const (
-	MetadataTableName = "cq:table_name"
-)
-
-type Schemas []*arrow.Schema
-
-func (s Schemas) Len() int {
-	return len(s)
-}
-
-func (s Schemas) SchemaByName(name string) *arrow.Schema {
-	for _, sc := range s {
-		tableName, ok := sc.Metadata().GetValue(MetadataTableName)
-		if !ok {
-			continue
-		}
-		if tableName == name {
-			return sc
-		}
-	}
-	return nil
-}
-
-func NewSchemasFromBytes(b [][]byte) (Schemas, error) {
+func NewSchemasFromBytes(b [][]byte) ([]*arrow.Schema, error) {
 	ret := make([]*arrow.Schema, len(b))
 	for i, buf := range b {
 		rdr, err := ipc.NewReader(bytes.NewReader(buf))
