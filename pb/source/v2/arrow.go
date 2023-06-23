a7399f57a6f612f579321b0dedf11e425f1e6a32
Yevgeny Pats <yev.pats@gmail.com>
2023-06-23 16:10:51 +0300

    feat: Add arrow schema encoding to v2 (#26)
    This moves byte encoding/decoding function to be under the right package/version (for old protocol) as they are part of the protocol and cannot change.

---
+++ pb/source/v2/arrow.go
@@ -0,0 +1,44 @@
+package source
+
+import (
+	"bytes"
+
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/ipc"
+)
+
+const (
+	MetadataTableName = "cq:table_name"
+)
+
+type Schemas []*arrow.Schema
+
+func (s Schemas) Len() int {
+	return len(s)
+}
+
+func (s Schemas) SchemaByName(name string) *arrow.Schema {
+	for _, sc := range s {
+		tableName, ok := sc.Metadata().GetValue(MetadataTableName)
+		if !ok {
+			continue
+		}
+		if tableName == name {
+			return sc
+		}
+	}
+	return nil
+}
+
+func (s Schemas) Encode() ([][]byte, error) {
+	ret := make([][]byte, len(s))
+	for i, sc := range s {
+		var buf bytes.Buffer
+		wr := ipc.NewWriter(&buf, ipc.WithSchema(sc))
+		if err := wr.Close(); err != nil {
+			return nil, err
+		}
+		ret[i] = buf.Bytes()
+	}
+	return ret, nil
+}
