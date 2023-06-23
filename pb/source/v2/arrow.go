66e9ca7380ad85378d5c9c60e09ac74ceb967911
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-06-23 16:41:32 +0300

    fix: Update schema encoding/deocoding in v2

--- pb/source/v2/arrow.go
+++ pb/source/v2/arrow.go
@@ -7,32 +7,9 @@ import (
 	"github.com/apache/arrow/go/v13/arrow/ipc"
 )
 
-const (
-	MetadataTableName = "cq:table_name"
-)
-
-type Schemas []*arrow.Schema
-
-func (s Schemas) Len() int {
-	return len(s)
-}
-
-func (s Schemas) SchemaByName(name string) *arrow.Schema {
-	for _, sc := range s {
-		tableName, ok := sc.Metadata().GetValue(MetadataTableName)
-		if !ok {
-			continue
-		}
-		if tableName == name {
-			return sc
-		}
-	}
-	return nil
-}
-
-func (s Schemas) Encode() ([][]byte, error) {
-	ret := make([][]byte, len(s))
-	for i, sc := range s {
+func SchemasToBytes(schemas []*arrow.Schema) ([][]byte, error) {
+	ret := make([][]byte, len(schemas))
+	for i, sc := range schemas {
 		var buf bytes.Buffer
 		wr := ipc.NewWriter(&buf, ipc.WithSchema(sc))
 		if err := wr.Close(); err != nil {
