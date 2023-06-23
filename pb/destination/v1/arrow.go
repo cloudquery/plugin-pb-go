2384690fd613f7c4ff12a3b8237f18b784fe2835
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-06-23 16:04:20 +0300

    feat: Add arrow schema encoding to v2

---
+++ pb/destination/v1/arrow.go
@@ -0,0 +1,43 @@
+package destination
+
+import (
+	"bytes"
+
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/ipc"
+)
+
+const (
+	MetadataTableName        = "cq:table_name"
+)
+
+type Schemas []*arrow.Schema
+
+func (s Schemas) Len() int {
+	return len(s)
+}
+
+func (s Schemas) SchemaByName(name string) *arrow.Schema {
+	for _, sc := range s {
+		tableName, ok := sc.Metadata().GetValue(MetadataTableName)
+		if !ok {
+			continue
+		}
+		if tableName == name {
+			return sc
+		}
+	}
+	return nil
+}
+
+func NewSchemasFromBytes(b [][]byte) (Schemas, error) {
+	ret := make([]*arrow.Schema, len(b))
+	for i, buf := range b {
+		rdr, err := ipc.NewReader(bytes.NewReader(buf))
+		if err != nil {
+			return nil, err
+		}
+		ret[i] = rdr.Schema()
+	}
+	return ret, nil
+}
