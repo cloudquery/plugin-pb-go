fee40aef3c4a28f66061a9a18fc90d548f0f590c
erezrokah <erezrokah@users.noreply.github.com>
2023-07-12 18:03:05 +0200

    style: Fix linting

--- managedplugin/random.go
+++ managedplugin/random.go
@@ -15,9 +15,9 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randSeq(n int) string {
 	b := make([]rune, n)
-	rand.Seed(time.Now().UnixNano())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[r.Intn(len(letters))]
 	}
 	return string(b)
 }
